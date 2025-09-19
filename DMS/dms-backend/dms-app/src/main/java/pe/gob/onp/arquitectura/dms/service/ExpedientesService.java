3f62e4309518b40223db84bd203bd086b5079e34
carlosormeno <carlos.ormeno@outlook.es>
2025-09-18 21:40:15 -0500

    desde mac

--- DMS/dms-backend/dms-app/src/main/java/pe/gob/onp/arquitectura/dms/service/ExpedientesService.java
+++ DMS/dms-backend/dms-app/src/main/java/pe/gob/onp/arquitectura/dms/service/ExpedientesService.java
@@ -1,6 +1,7 @@
 
 package pe.gob.onp.arquitectura.dms.service;
 
+import pe.gob.onp.arquitectura.dms.api.dto.DocumentoDtos;
 import pe.gob.onp.arquitectura.dms.api.dto.ExpedienteDtos.*;
 import reactor.core.publisher.Mono;
 
@@ -14,4 +15,12 @@ public interface ExpedientesService {
     Mono<String> ensureExpedienteFolder(String expedienteCodigo);
 
     List<CarpetaBase> getCarpetasBase();
+
+    DocumentoDtos.UploadResponseCompleto uploadDocumento(String expedienteId, DocumentoDtos.UploadDocumentoRequest request);
+
+    PageDocumento<DocumentoInfo> getDocumentos(String expedienteId, String subcarpeta, int page, int size);
+
+    List<DocumentoDtos.SubcarpetaInfo> getSubcarpetas(String expedienteId);
+
+    boolean isSubcarpetaValida(String subcarpeta);
 }
